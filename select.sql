da7f830c679e2290ee7f0bd84af1cf33fc477d14
Neto test <skilful1010@mail.ru>
2025-03-09 22:30:22 +0300

    Correction create.sql and select.sql

--- select.sql
+++ select.sql
@@ -12,12 +12,12 @@ where length = (select max(length) from musics);
 --Название треков, продолжительность которых не менее 3,5 минут.
 select name, length
 from musics
-where length > '00:03:30';
+where length >= '00:03:30';
 
 --Названия сборников, вышедших в период с 2018 по 2020 год включительно.
 select name
 from colections
-where year between 2018 and 2020;
+where colection_year >= '2018-01-01' and colection_year < '2021-01-01';
 
 --Исполнители, чьё имя состоит из одного слова.
 select name 
@@ -27,34 +27,49 @@ where name not like '% %';
 --Название треков, которые содержат слово «мой» или «my».
 select name
 from musics
-where name like '%мой%' or name like '%my%';
+where LOWER(name) like '%мой %' 
+or LOWER(name) like '% мой %'
+or LOWER(name) like '% мой%'
+or LOWER(name) like '%my %'
+or LOWER(name) like '% my %'
+or LOWER(name) like '% my%'
+and LOWER(name) not like '%мой%'
+and LOWER(name) not like '%my%';
 
 --Количество исполнителей в каждом жанре.
 select name, count(musician_id) from musiciansstyles ms
 join styles s on id = ms.style_id
-group by s.name; --совсем не понял как получилось...
+group by s.name;
 
 --Количество треков, вошедших в альбомы 1995–2001 годов.
 --Я поменял дату в условии, чтобы не менять в БД
-select a.name, count(m.id) from musics m
+select count(m.id) from musics m
 join albums a on a.id = m.album_id
-where a.data between '1995-01-01' and '2002-01-01'
-group by a.name; --почти понял
+where a.album_date between '1995-01-01' and '2002-01-01';
 
 --Средняя продолжительность треков по каждому альбому.
 select a.name, AVG(m.length) from musics m
 join albums a on a.id = m.album_id
-group by a.name; --почти понял
+group by a.name;
 
---Все исполнители, которые не выпустили альбомы в 2020 году.
-select m.name from musicians m
-join albumsmusicians am on id = am.musician_id
-join albums a on a.id = am.album_id
-where data < '1995-01-01' or data > '1996-01-01'
-group by m.name; --не разобрался
+--Все исполнители, которые не выпустили альбомы в 1999 году.
+--Дату в условии изменил, чтобы не менять все даты в БД
+SELECT m.name
+FROM musicians m
+WHERE m.name  NOT IN (
+    SELECT ms.name
+    FROM musicians ms
+    JOIN albumsmusicians a ON a.musician_id = ms.id
+    JOIN albums a2 ON a2.id = a.album_id
+    WHERE a2.album_date >= '1999-01-01' and a2.album_date < '2000-01-01'
+);
 
 --Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
-select c.name from colections c
-join musicscolections mc on id = mc.colection_id
-join music m on m.id = mc.album_id
-group by c.name; --не разобрался
+SELECT DISTINCT c.name
+FROM colections c
+JOIN musicscolections mc ON mc.colection_id = c.id
+JOIN musics mus ON mus.id = mc.music_id
+JOIN albums a ON a.id = mus.album_id
+JOIN albumsmusicians am ON am.album_id = a.id
+JOIN musicians m ON m.id = am.musician_id
+WHERE m.name = 'Король и Шут';
